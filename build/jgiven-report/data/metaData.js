d6f202dec4ec10e3edd98424f22b7d1bbb7d52ee
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-09-04 21:14:17 +0000

    Deploy daggerok/spring-boot-oauth2 to github.com/daggerok/spring-boot-oauth2.git:gh-pages

--- build/jgiven-report/data/metaData.js
+++ build/jgiven-report/data/metaData.js
@@ -1,1 +1,1 @@
-jgivenReport.setMetaData({"created":"Sep 4, 2018 5:56:21 PM","version":"0.15.3-22e6c60d89d9","title":"JGiven Report","data":[],"showThumbnails":true} );
+jgivenReport.setMetaData({"created":"Sep 4, 2018 9:10:41 PM","version":"0.15.3-22e6c60d89d9","title":"JGiven Report","data":[],"showThumbnails":true} );
